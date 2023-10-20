36e1b1d6ff4582b9179ca644d709c5ce9fe124c3
catalinh <catalin.hercut@scgroup.ch>
2023-10-20 13:58:58 +0200

    fixed folder structure

--- src/app/[...page]/page.tsx
+++ src/app/[...page]/page.tsx
@@ -18,6 +18,8 @@ export default async function Page(props: PageProps) {
         // Use the page path specified in the URL to fetch the content
         urlPath: "/" + (props?.params?.page?.join("/") || ""),
       },
+      // Set prerender to false to return JSON instead of HTML
+      prerender: false,
     })
     // Convert the result to a promise
     .toPromise();
